3fed777c4774943931637b9323b3b73033160f44
christian.staeblein <christian.staeblein@gmail.com>
2026-09-15 21:13:58 +0100

    We have a player!

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import constants
+import player
 from logger import log_state
 
 def main():
@@ -11,7 +12,7 @@ def main():
     clock = pygame.time.Clock()
     dt = 0.0
 
-
+    p = player.Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
 
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
@@ -20,8 +21,12 @@ def main():
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 return
+
         screen.fill(pygame.Color("black"))
+        p.draw(screen)
         pygame.display.flip()
+
+
 
         dt= clock.tick(60)/1000
 
