4155a964bae8d172232223822b273bb1a39e3032
christian.staeblein <christian.staeblein@gmail.com>
2026-09-15 20:48:02 +0100

    limit fps via clock

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
+
+
+
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
     while True:
@@ -16,6 +22,8 @@ def main():
                 return
         screen.fill(pygame.Color("black"))
         pygame.display.flip()
+
+        dt= clock.tick(60)/1000
 
 
 if __name__ == "__main__":
